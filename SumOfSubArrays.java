737071dd5a5eb62aa2c16095581440729f4bc1ac
Portal Creator <prachiikhar42@gmail.com>
2025-07-24 00:52:14 +0530

    Sum of Subarrays

--- SumOfSubArrays.java
+++ SumOfSubArrays.java
@@ -1,5 +1,18 @@
 public class SumOfSubArrays {
+	public static int sumofsubarray(int arr[]) {
+		// arr[i]kitney baar aayega? har sub arraya mai-->n-i times
+		// aur It can be part of subarrays starting from index 0 to i → (i + 1) choices
+		int n = arr.length;
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum += arr[i] * (n - i) * (i + 1);
+		}
+		return sum;
+	}
+
 	public static void main(String args[]) {
 		int arr[] = { 1, 2, 3 };
+		int ans = sumofsubarray(arr);
+		System.out.println(ans);
 	}
 }
