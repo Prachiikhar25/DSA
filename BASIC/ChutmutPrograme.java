5a881c0d1584ff5bc70380e1c949b4261e30a87d
Portal Creator <prachiikhar42@gmail.com>
2025-08-06 08:14:59 +0530

    Added  String function

--- BASIC/ChutmutPrograme.java
+++ BASIC/ChutmutPrograme.java
@@ -24,8 +24,34 @@ public class ChutmutPrograme {
         System.out.println("GCD BY EUCLIDEAN IS " + a);
     }
 
+    static void CheckPalindrome(String s) {
+        String st = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int n = st.length();
+        for (int i = 0; i < st.length(); i++) {
+            if (st.charAt(i) != st.charAt(n - 1 - i)) {
+                System.out.println("nahi hai ");
+                break;
+            }
+        }
+        System.out.println("Hai pali");
+        System.out.println(s);
+        System.out.println(st);
+    }
+
+    static void CompareString(String s1, String s2) {
+        if (s1.compareTo(s2) == 0) {
+            System.out.println("Case ni dekhte compare to");
+        } else {
+            System.out.println("Case dekhte hai");
+        }
+
+    }
+
     public static void main(String args[]) {
         GCD(27, 90);
         EuclideanGCD(12, 18);
+        CheckPalindrome("Too hot to hoot");
+        CompareString("HI", "hi");
+
     }
 }
